b244e219b565e8e93b02c2a8e55c273e82fc2581
200901002 <200901002@rajalakshmi.edu.in>
2023-09-16 19:49:39 +0530

    aboutV2

---
+++ aboutV2/src/Search.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import ReactMarkdown from "react-markdown";
+import "./App.scss";
+const lookupTable = {
+  oali: "Analog Electronics",
+  basc: "Fundamentals",
+  embs: "Embedded Systems",
+  psop: "Power system operation",
+  fats: "Flexible AC Transmission Systems",
+  rees: "Renewable Energy Systems",
+  hevs: "Hybrid Electric Vehicles",
+  bect: "Circuit Theory",
+  coel: "Consumer Electronics",
+  cosy: "Control Systems",
+  diel: "Digital Electronics",
+  doem: "Design Of Electrical Machines",
+  dsip: "Digital Signal Processing",
+  edac: "Electronic Devices And Circuits",
+  emfi: "Electromagnetic Fields",
+  mach: "Electrical Machines",
+  main: "Measurement and Instrumentation",
+  mpmc: "Microprocessors",
+  meeg: "Mechanical Engineering",
+  phys: "Physics",
+  plsc: "PLC and Scada",
+  poel: "Power Electronics",
+  posy: "Power Systems Analysis",
+  prsw: "Protection And Switch Gear",
+  slsd: "Solid State Drives",
+  spem: "Special Machines",
+  tmdt: "Transmission And Distribution",
+};
+
+function SearchAGP() {
+   <>
+   <h1>dad</h1>
+   </>
+}
+
+export default SearchAGP;
