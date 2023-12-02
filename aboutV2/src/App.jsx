00cf0c838776afc5405fa6779f49d8cc704ac7d1
Adhithya03 <trainadhi03@gmail.com>
2023-12-02 19:46:58 +0530

    Add WordCloudComponent and update chart title

--- aboutV2/src/App.jsx
+++ aboutV2/src/App.jsx
@@ -1,20 +1,20 @@
 import { Content, Theme } from "@carbon/react";
 import NavHeader from "./Header";
 import BarChartComponent from "./stats";
+// import WordCloudComponent from "./stats";
 import AboutAirgapfluxPage from "./About";
-
 
 function App() {
   return (
     <>
       <Theme theme="g100">
         <NavHeader />
-      </Theme >
-      <Theme >
+      </Theme>
+      <Theme>
         <Content>
-            <BarChartComponent />
-
-            <AboutAirgapfluxPage />
+          <BarChartComponent />
+          {/* <WordCloudComponent /> */}
+          <AboutAirgapfluxPage />
         </Content>
       </Theme>
     </>
--- aboutV2/src/stats.jsx
+++ aboutV2/src/stats.jsx
@@ -1,39 +1,36 @@
-import React, { Component } from 'react';
-import { SimpleBarChart } from '@carbon/charts-react';
-import '@carbon/charts/styles.css';
-
+import React, { Component } from "react";
+import { SimpleBarChart } from "@carbon/charts-react";
+import "@carbon/charts/styles.css";
 
 const lookupTable = {
-    oali: "Analog Electronics",
-    basc: "Fundamentals",
-    embs: "Embedded Systems",
-    psop: "Power system operation",
-    fats: "Flexible AC Transmission Systems",
-    rees: "Renewable Energy Systems",
-    hevs: "Hybrid Electric Vehicles",
-    bect: "Circuit Theory",
-    coel: "Consumer Electronics",
-    cosy: "Control Systems",
-    diel: "Digital Electronics",
-    doem: "Design Of Electrical Machines",
-    dsip: "Digital Signal Processing",
-    edac: "Electronic Devices And Circuits",
-    emfi: "Electromagnetic Fields",
-    mach: "Electrical Machines",
-    main: "Measurement and Instrumentation",
-    mpmc: "Microprocessors",
-    meeg: "Mechanical Engineering",
-    phys: "Physics",
-    plsc: "PLC and Scada",
-    poel: "Power Electronics",
-    posy: "Power Systems Analysis",
-    prsw: "Protection And Switch Gear",
-    slsd: "Solid State Drives",
-    spem: "Special Machines",
-    tmdt: "Transmission And Distribution",
-  };
-  
-
+  oali: "Analog Electronics",
+  basc: "Fundamentals",
+  embs: "Embedded Systems",
+  psop: "Power system operation",
+  fats: "Flexible AC Transmission Systems",
+  rees: "Renewable Energy Systems",
+  hevs: "Hybrid Electric Vehicles",
+  bect: "Circuit Theory",
+  coel: "Consumer Electronics",
+  cosy: "Control Systems",
+  diel: "Digital Electronics",
+  doem: "Design Of Electrical Machines",
+  dsip: "Digital Signal Processing",
+  edac: "Electronic Devices And Circuits",
+  emfi: "Electromagnetic Fields",
+  mach: "Electrical Machines",
+  main: "Measurement and Instrumentation",
+  mpmc: "Microprocessors",
+  meeg: "Mechanical Engineering",
+  phys: "Physics",
+  plsc: "PLC and Scada",
+  poel: "Power Electronics",
+  posy: "Power Systems Analysis",
+  prsw: "Protection And Switch Gear",
+  slsd: "Solid State Drives",
+  spem: "Special Machines",
+  tmdt: "Transmission And Distribution",
+};
 
 class BarChartComponent extends Component {
   constructor(props) {
@@ -42,20 +39,18 @@ class BarChartComponent extends Component {
     this.state = {
       data: [],
       options: {
-        title: 'Number of Video Resources by Subject',
-      
-        
+        title: "Number of Video Resources by Subject",
+
         axes: {
           left: {
-            mapsTo: 'group',
-            scaleType: 'labels',
+            mapsTo: "group",
+            scaleType: "labels",
           },
           bottom: {
-            mapsTo: 'value',
-
+            mapsTo: "value",
           },
         },
-        height: '1000px',
+        height: "1000px",
       },
     };
   }
@@ -65,10 +60,10 @@ class BarChartComponent extends Component {
   }
 
   fetchData = () => {
-    fetch('https://api.airgapflux.in/fetch/videos.php')
+    fetch("https://api.airgapflux.in/fetch/videos.php")
       .then((response) => {
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
         }
         return response.json();
       })
@@ -76,42 +71,42 @@ class BarChartComponent extends Component {
         this.setState({ data: this.processData(data) });
       })
       .catch((error) => {
-        console.error('Failed to fetch data:', error);
+        console.error("Failed to fetch data:", error);
       });
   };
   processData = (data) => {
     const subjectResourceCounts = data.reduce((acc, item) => {
-        const subjectAcronym = item.Subject || 'Unknown'; // Acronym or 'Unknown'
-        if (subjectAcronym === 'Unknown') {
-            console.log(`${item.Subject}`);
-        }
-    // log unknown subject
+      const subjectAcronym = item.Subject || "Unknown"; // Acronym or 'Unknown'
+      if (subjectAcronym === "Unknown") {
+        console.log(`${item.Subject}`);
+      }
+      // log unknown subject
       const subject = lookupTable[subjectAcronym] || subjectAcronym; // Use lookup table or the acronym itself
       acc[subject] = (acc[subject] || 0) + 1;
       return acc;
     }, {});
-  
+
     let chartData = Object.keys(subjectResourceCounts).map((subject) => ({
       group: subject,
       value: subjectResourceCounts[subject],
     }));
-  
+
     // Sort chart data by the value field (the amount of resources)
-    chartData.sort((a, b) =>  a.value - b.value);
-  
+    chartData.sort((a, b) => a.value - b.value);
+
     return chartData;
   };
-  
+
   render() {
     return (
       <div>
-        <br /> 
-        <br /> 
+        <br />
+        <br />
         <h3>Resource Distribution Chart</h3>
         <p>
-          The below is a chart of the number of resources available for each subject in our website database. 
-          
-          The data is fetched from the database and is updated each time you refresh this page.
+          The below is a chart of the number of resources available for each
+          subject in our website database. The data is fetched from the database
+          and is updated each time you refresh this page.
         </p>
         <SimpleBarChart data={this.state.data} options={this.state.options} />
       </div>
